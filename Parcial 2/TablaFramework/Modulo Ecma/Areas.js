e07a400ffdda4c29ff697865eef25641d3fabf95
Alan Arratia <halan3@hotmail.com>
2023-10-03 19:58:23 -0500

    Commit Modulo Ecma Terminado

---
+++ Parcial 2/TablaFramework/Modulo Ecma/Areas.js
@@ -0,0 +1,19 @@
+export function areacuadrado(lado) {
+    return lado * lado;
+}
+
+export function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+
+export function areaCirculo(radio) {
+    return Math.PI * radio * radio;
+}
+
+export function areaRectangulo(ancho, alto) {
+    return ancho * alto;
+}
+
+
+
+//console.log(module);
